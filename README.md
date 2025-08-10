2ec14dedf6a953ab180aa5ef06280821bca0bbc8
onajifortune <onajifortunejnr@gmail.com>
2025-08-10 20:11:22 +0100

    Added Namada chart and its dependencies

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Namada
+
 Governance and Network Metrics Dashboard
+
+## 🚀 Setup
+
+To set up the development environment, follow these steps:
+
+1. **Install the project dependencies:**
+
+   ```bash
+   npm install
+
+   ```
+
+2. **Run the development server:**
+   ```bash
+   npm run dev
+   ```
+
+## Learn More
+
+Check out [vite.dev](https://vite.dev/) to learn more about Vite.
---
+++ src/components/Charts/ExportButton.tsx
@@ -0,0 +1,17 @@
+import Button from "../Button/Button";
+
+interface ExportButtonProps {
+  handleSaveToPng: (imgLabel: string) => void;
+  imgLabel: string;
+}
+export function ExportButton(props: ExportButtonProps) {
+  return (
+    <div className="flex justify-between items-center text-sm text-gray-500">
+      <Button
+        text="Export PNG"
+        className="bg-[#1984c7] text-white px-4 py-2 rounded-md mt-2 sm:mt-0 hover:bg-[#1675b1] transition"
+        onClick={() => props.handleSaveToPng(props.imgLabel)}
+      />
+    </div>
+  );
+}
